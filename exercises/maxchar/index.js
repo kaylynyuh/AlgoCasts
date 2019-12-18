1011f0762c8f316c3708ca01d0808770e07bcffa
kaylynyuh <yuhkay4@gmail.com>
2019-12-17 19:29:06 -0800

    maxchars wip

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,7 +4,24 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
+// You can use the following technique for similar type questions:
+// What is the most common character in the string?
+// Does string A have the same characters as string B (is it an anagram?)
+// Does the given string have any repeated characters in it?
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  for (let char of str) {
+    // if charMap[char] already exists, increment
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      // else if charMap[char] does NOT exist, set
+      // its value to 1
+      charMap[char] = 1;
+    }
+  }
+  console.log(charMap);
+}
 
 module.exports = maxChar;

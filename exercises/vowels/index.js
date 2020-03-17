1b7f0665e5139dfc2b9ecca60586cdd50370b45a
kaylynyuh <yuhkay4@gmail.com>
2020-03-17 15:42:31 -0700

    vowels wip

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0;
+  const valid = 'aeiou';
+  for(let char of str.toLowerCase()) {
+    if(valid.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = vowels;

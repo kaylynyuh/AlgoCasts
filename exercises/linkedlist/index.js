b6a07f65a682a6e13ba35c6d3af8605fe983e497
kaylynyuh <yuhkay4@gmail.com>
2020-09-28 12:55:56 -0700

    building out linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -114,9 +114,98 @@ class LinkedList {
     if (index === 0) {
       this.head = new Node(data, this.head)
     }
-    const previous = this.getAt(index -1 ) || this.getLast()
+    const previous = this.getAt(index - 1) || this.getLast()
     const node = new Node(data, previous.next)
     previous.next = node
+  }
+
+  // remove dups in a SORTED linked list
+  removeDuplicates(head) {
+    let current = head
+    while (current && current.next) {
+      if (current.data === current.next.data) {
+        current = current.next.next
+      } else {
+        current.next
+      }
+    }
+    return head
+  }
+
+  // remove dups in an UNSORTED linked list
+  // caveat: only works if you are allowed to use
+  // additional data structures to reach your solution
+  removeDuplicates() { // O(n)
+    let p1 = this.head
+    let p2 = p1.next
+    let nodes = {}
+    node[p1.data] = true
+    while (p2) {
+      let data = p2.data
+      if (nodes[data]) {
+        p1.next = p2.next
+      } else {
+        nodes[data] = true
+        p1 = p2
+      }
+      p2 = p2.next
+    }
+  }
+
+  // remove dups in an UNSORTED linked list
+  // if you are NOT allowed to use
+  // additional data structures to reach your solution
+  removeDuplicates() { // O(n^2)
+    let p1, p2, p3;
+    let val = null;
+    while (p2) {
+      val = p2.data;
+      p1 = p2;
+      p3 = p1.next;
+  
+      while (p3) {
+        if (p3.data === val) {
+            p1.next = p3.next;
+        } else {
+          p1 = p3;
+        }
+        p3 = p3.next;
+      }
+  
+      p2 = p2.next;
+    }
+  }
+
+  // The solution your interviewer is likely after is to have 
+  // two pointers; one that starts iterating through the list 
+  // immediately, and one that is delayed kk times before iterating. 
+  // Once the first pointer reaches the end, the second pointer will 
+  // either be the kkth last element, or it will not be initialised, 
+  // meaning that the list was not of sufficient length.
+  kthToLast(k) {
+    let p1 = this.head
+    let p2 = this.head
+    for (let i = 0; i < k - 1; i++) {
+      if (!p2.next) return null;
+      p2 = p2.next;
+    }
+    while (p2.next) {
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+    return p1.data;
+  }
+
+  deleteMiddle() {
+    let p1 = this.head
+    let p2 = this.head.next
+    while (p2) {
+      p1 = p1.next
+      p2 = p2.next.next
+      if (!p2.next) {
+        p1.next = p2.next
+      }
+    }
   }
 }
 

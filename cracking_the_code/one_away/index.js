b57a5f8e4425f3599f5bb7f484a89454444073c2
kaylynyuh <yuhkay4@gmail.com>
2020-10-01 16:25:45 -0700

    reorganizing folder structure

---
+++ cracking_the_code/one_away/index.js
@@ -0,0 +1,9 @@
+// There are 3 types of edits that can be perfromed on a string: 
+// insert a char, remove a char, or replace a char. Given two strings,
+// write a function to check if they are 1 or 0 edits away.
+
+// This is one of those problems where it's helpful to think about
+// to think about the meaning of each of these operations. What
+// does it mean for 2 strings to be one insertion, removal, or 
+// replacement away from each other?
+

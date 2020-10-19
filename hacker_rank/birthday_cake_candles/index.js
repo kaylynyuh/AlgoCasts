7992a5d7d0bace6c5c662cf0d2685fa379c86b0a
kaylynyuh <yuhkay4@gmail.com>
2020-10-19 10:20:04 -0700

    diagonal difference

--- hacker_rank/birthday_cake_candles/index.js
+++ hacker_rank/birthday_cake_candles/index.js
@@ -1,7 +1,13 @@
-// Problem Domain: https://www.hackerrank.com/challenges/birthday-cake-candles/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+// Problem Domain: https://www.hackerrank.com/challenges/birthday-cake-candles
 
 function birthdayCakeCandles(candles) {
-  let max = Math.max(...ar);    
-  let result = candles.filter(c => c === max);    
-  return result.length;
+  let sorted = candles.sort();
+  let max = candles[sorted.length - 1];
+  let maxFrequency = 0;
+  for (let i = 0; i < sorted.length; i++) {
+      if (sorted[i] === max) {
+          maxFrequency++;
+      }
+  }
+  return maxFrequency;
 }
---
+++ hacker_rank/diagonal_difference/index.js
@@ -0,0 +1,22 @@
+// Problem Domain: https://www.hackerrank.com/challenges/diagonal-difference/problem
+
+// Since we know that the first set of diagonal values are at (0, 0), (1, 1), (2, 2)
+// then we know the check we need to queue off of inside our nested loop is if 
+// row === col. Since we also know that the inverse of that (2, 2), (1, 1), (0, 0)
+// points to the second set of diagonal values, than we know we can simply reverse
+// the first set of diagonal values and then calculate their absolute difference
+
+function diagonalDifference(arr) {
+  let diagonalFirst = [];
+  let diagonalSecond = [];
+  for (let row = 0; row < arr.length; row++) {
+    for (let col = 0; col < arr.length; col++) {
+      if (row === col) {
+        diagonalFirst.push(arr[row][col]);
+        diagonalSecond.unshift(arr[row][col])
+      }
+    }
+  }
+  console.log('diagonalFirst', diagonalFirst)
+  console.log('diagonalSecond', diagonalSecond)
+}

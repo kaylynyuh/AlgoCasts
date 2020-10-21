28b65508c0106d0d6b21a0ff49da31f1732d2557
kaylynyuh <yuhkay4@gmail.com>
2020-10-21 13:33:46 -0700

    sorted insert, double LL

--- exercises/linkedlist_doubly/index.js
+++ exercises/linkedlist_doubly/index.js
@@ -24,4 +24,35 @@ class DoublyLinkedList {
     }
     this.tail = newNode;
   }
+
+  // insert a new node into a sorted list
+  // such the new node's data maintains the sort order
+  sortedInsert(head, data) {
+    let newNode = new DoublyLinkedListNode(data);
+    if (!head) { // Insert in empty list
+        return newNode;
+    } else if (data < head.data) { // Insert in front of list
+        newNode.next = head;
+        head.prev = newNode;
+        return newNode;
+    } else {
+        /* Walk list with 2 pointers */
+        let n1 = null;
+        let n2 = head;
+        while (n2 !== null && n2.data < data) {
+            n1 = n2;
+            n2 = n2.next;
+        }
+        if (n2 === null) { // Insert at end of list
+            n1.next = newNode;
+            newNode.prev = n1;
+        } else { // insert in empty list
+            n1.next = newNode;
+            n2.prev = newNode;
+            newNode.prev = n1;
+            newNode.next = n2;
+        }
+        return head;
+    } 
+  }
 }

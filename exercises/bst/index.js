e8b92a6a2f0f8c5615ffc173cfbd377959db4a3e
kaylynyuh <yuhkay4@gmail.com>
2020-11-09 11:28:46 -0800

    bst wip

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -44,6 +44,51 @@ class Node {
     }
     return null;
   }
+
+  remove(data) {
+    this.root = this.removeNode(this.root, data)
+  }
+
+  removeNode(node, key) {
+    // if the root is null, then the tree is empty, return null
+    if (node === null) return null;
+    // if data to be delete is less than  
+    // roots data then move to left subtree 
+    if (key < node.data) {
+      node.left = this.removeNode(node.left, key)
+      return node
+    } 
+    // if data to be delete is greater than  
+    // roots data then move to right subtree 
+    else if (key > node.data) {
+      node.right = this.removeNode(node.right, key)
+    } 
+    // if data is similar to the root's data  
+    // then delete this node
+    else {
+      // deleting the node with no children
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node
+      }
+      // deleting node with one child 
+      if (node.left === null) {
+        node = node.right
+        return node
+      } else if (node.right === null) {
+        node = node.left
+        return node
+      }
+      // deleting node with two children 
+      // minumum node of the rigt subtree 
+      // is stored in aux 
+      var aux = this.findMinNode(node.right); 
+      node.data = aux.data; 
+  
+      node.right = this.removeNode(node.right, aux.data); 
+      return node; 
+    }
+  }
 }
 
 module.exports = Node;
